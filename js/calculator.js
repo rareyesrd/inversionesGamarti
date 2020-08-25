de3ec05be790f7a90e8ad9ddddf72ec587b83d2e
Rafael Reyes <r.reyes@caribemedia.com.do>
2020-08-25 16:19:05 -0400

    Adding gastoCierre and totalPrestamo methods to the calculator

--- js/calculator.js
+++ js/calculator.js
@@ -1,6 +1,26 @@
 var app = new Vue({
     el: '#calculator',
     data: {
-      message: 'Hello Vue!'
+      error: [],
+      amount: null,
+      vehiculo: null,
+      cierre: null,
+      interes: null,
+      meses: null,
+      prestamo: null
+    },
+    methods:{
+      gastoCierre: function(){
+        this.cierre = parseInt(this.amount) * 0.1;
+        return this.cierre;
+      },
+      totalPrestamo: function(){
+         this.prestamo = parseInt(this.amount) + this.gastoCierre();
+         return this.prestamo;
+      },
+      submit: function (e) {
+        this.gastoCierre();
+        this.totalPrestamo();
+      }
     }
   })
