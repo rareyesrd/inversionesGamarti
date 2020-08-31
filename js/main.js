f3ecb67fe565c1273a6d6507898f1bd470fbf948
Rafael Reyes <r.reyes@caribemedia.com.do>
2020-08-31 16:29:46 -0400

    Accordion done

--- js/main.js
+++ js/main.js
@@ -1,9 +1,86 @@
-let spand = document.querySelectorAll(".card-header");
-for (let el of spand) {
-  el.addEventListener("click", () => {
-    let toogleSign = el.querySelector(".spand").children;
-    for (const index of toogleSign) {
-      index.classList.toggle("active");
-    }
-  });
-}
+// let spand = document.querySelectorAll(".card-header");
+// for (let el of spand) {
+//   el.addEventListener("click", () => {
+//     let toogleSign = el.querySelector(".spand").children;
+//     for (const index of toogleSign) {
+//       index.classList.toggle("active");
+//     }
+//   });
+// }
+
+Vue.component("zz-accordion", {
+  props: ["contents"],
+  data: function () {
+    return {};
+  },
+  methods: {
+    expand: function (i) {
+      let el = this.$refs["accordion-body-" + i][0];
+
+      if (this.contents[i].active === false) {
+        this.contents[i].active = true;
+
+        TweenLite.to(el, 1, {
+          height: el.scrollHeight,
+          ease: Elastic.easeOut.config(1, 0.3),
+        });
+      } else {
+        this.contents[i].active = false;
+
+        TweenLite.to(el, 0.5, {
+          height: 0,
+          ease: Bounce.easeOut,
+        });
+      }
+    },
+  },
+
+  template: `
+<div class="accordion">
+	<div
+		class="accordion-item"
+		v-for="content, i in contents"
+		v-bind:class="{ 'accordion-active': content.active }"
+	>
+		<div class="accordion-header">
+			<a href="#" v-on:click.prevent="expand(i)">
+				<div class="accordion-header-div">{{ content.title }}</div>
+				<div class="accordion-header-div">
+					<div class="accordion-caret"></div>
+				</div>
+			</a>
+		</div>
+		<div class="accordion-body" v-bind:ref="'accordion-body-' + i">
+			<div class="accordion-content">{{ content.description }}</div>
+		</div>
+	</div>
+</div>
+`,
+});
+
+// ----------
+
+var app = new Vue({
+  el: "#app",
+  data: {
+    contents: [
+      {
+        title: "Misión",
+        description:
+          "Ofrecer soluciones financieras a tiempo, con la calidad humana, comodidad y la rapidez que nos caracteriza, con la finalidad de satisfacer y llenar las expectativas de cada cliente.",
+        active: false,
+      },
+      {
+        title: "Visión",
+        description:
+          "Ser la opción preferida en soluciones financieras, convirtiendo las oportunidades en realidad, a través de un equipo calificado comprometido con las metas de nuestros clientes.",
+      },
+      {
+        title: "Valores",
+        description:
+          "Responsabilidad, Respeto, Integridad, Empatía, Innovación",
+        active: false,
+      },
+    ],
+  },
+});
